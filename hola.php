e3ac41ca53ee7a1fabfb20fd1a3d5e2122fb0beb
David Elena <davidemari2001@gmail.com>
2022-05-21 12:03:00 +0000

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,4 +1,5 @@
-//David Elena Heredia
 <?php
-@print "Hola {$argv[1]}\n";
-?>
+// Autor: David Elena Heredia - davidemari2001@gmail.com
+// El nombre por defecto es Mundo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";
